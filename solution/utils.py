7381d3bfea63fcd36e17c89188d5be0e692559d4
nikolaims <n.m.smetanin@gmail.com>
2021-10-16 19:41:53 +0300

    apply model util

--- solution/utils.py
+++ solution/utils.py
@@ -1,6 +1,8 @@
 import sounddevice
 
-from solution.data import SAMPLING_RATE
+from solution.data import SAMPLING_RATE, SAMPLES_LEN
+import torch
+import numpy as np
 
 
 def play(waveform, fs=SAMPLING_RATE):
@@ -10,3 +12,14 @@ def play(waveform, fs=SAMPLING_RATE):
     :param waveform: waveform numpy array
     """
     sounddevice.play(waveform, fs, blocking=True)
+
+
+def apply_model(x, model, transform, hop_ms):
+    p = np.zeros(len(x))
+    hop = int(hop_ms/1000*SAMPLING_RATE)
+    with torch.no_grad():
+        for start in range(0, len(x)-SAMPLES_LEN, hop):
+            x_slice = x[start:start+SAMPLES_LEN]
+            prob = torch.sigmoid(model(transform(x_slice.reshape(1, -1)))).item()
+            p[start:start + SAMPLES_LEN] += prob
+    return p/(SAMPLES_LEN/hop)
